554289760a71bad396bedb9a755cd1bde2a253c0
Serhii0080 <startap.chrom@gmail.com>
2026-03-24 17:52:40 +0200

    Fix Codewars badge

--- codewars/8kyu/animal.js
+++
@@ -1,9 +0,0 @@
-// 🍕 Задача  Даю вам функцию animal, принимающую 1 параметр: obj  Вот так:
-
-// ❗❗  {name:"dog",legs:4,color:"white"}  и вернет строку следующего вида: "This white dog has 4 legs."
-
-function animal(obj) {
-    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
-}
-
-console.log(animal({ name: "dog", legs: 4, color: "white" }));
--- codewars/8kyu/boolToWord.js
+++
@@ -1,7 +0,0 @@
-// 🍕 Дополните метод, который принимает логическое значение и возвращает "Yes"строку для true или "No"строку для false.
-
-function boolToWord(bool) {
-    return bool ? "Yes" : "No";
-}
-
-console.log(boolToWord(false));
